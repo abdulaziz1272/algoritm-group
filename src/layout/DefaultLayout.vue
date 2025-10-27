7bb3be47f342eab5c3b9cd23e88aa84f1b8d9998
ibrohim3 <ibrohim.2128382@gmail.com>
2025-10-27 23:22:39 +0500

    feat: add modern glassmorphism Footer component with social links and optimized icons

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -2,15 +2,18 @@
   <main>
     <Header />
     <router-view />
+    <Footer />
   </main>
 </template>
 <script>
+import Footer from "../components/Footer/Footer.vue";
 import Header from "../components/Header/Header.vue";
 import Home from "../pages/Home/Home.vue";
 export default {
   components: {
     Header,
     Home,
+    Footer,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import '../src/assets/CSS/style.css'
 import App from './App.vue'
 import router from './routes'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createApp(App)
     .use(router)
